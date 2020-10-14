68864d1163f786fc0ce688cb17b22fb08c9b86fe
Daniel Kiss <littlesnorrboy@gmail.com>
2020-10-14 20:45:35 +0200

    Refactor get_instruction_descriptions

--- cao-lang/src/compiler/description.rs
+++ cao-lang/src/compiler/description.rs
@@ -1,3 +1,4 @@
+use super::InstructionNode;
 use crate::scalar::Scalar;
 use crate::traits::ByteEncodeble;
 use crate::InputString;
@@ -8,39 +9,38 @@ use crate::{subprogram_description, SubProgram, SubProgramType};
 
 pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
     vec![
-        subprogram_description!(
-            "Add",
-            "Add two scalars",
-            SubProgramType::Instruction,
-            [Scalar, Scalar],
-            [Scalar],
-            []
-        ),
-        subprogram_description!(
-            "Sub",
-            "Subtract two scalars",
-            SubProgramType::Instruction,
-            [Scalar, Scalar],
-            [Scalar],
-            []
-        ),
-        subprogram_description!(
-            "Mul",
-            "Multiply two scalars",
-            SubProgramType::Instruction,
-            [Scalar, Scalar],
-            [Scalar],
-            []
-        ),
-        subprogram_description!(
-            "Div",
-            "Divide two scalars",
-            SubProgramType::Instruction,
-            [Scalar, Scalar],
-            [Scalar],
-            []
-        ),
-        subprogram_description!(
+        get_desc(InstructionNode::Start),
+        get_desc(InstructionNode::Pass),
+        get_desc(InstructionNode::Add),
+        get_desc(InstructionNode::Sub),
+        get_desc(InstructionNode::Mul),
+        get_desc(InstructionNode::Div),
+        get_desc(InstructionNode::CopyLast),
+        get_desc(InstructionNode::Less),
+        get_desc(InstructionNode::LessOrEq),
+        get_desc(InstructionNode::Equals),
+        get_desc(InstructionNode::NotEquals),
+        get_desc(InstructionNode::Pop),
+        get_desc(InstructionNode::ClearStack),
+        get_desc(InstructionNode::ScalarInt(Default::default())),
+        get_desc(InstructionNode::ScalarFloat(Default::default())),
+        get_desc(InstructionNode::ScalarArray(Default::default())),
+        get_desc(InstructionNode::StringLiteral(Default::default())),
+        get_desc(InstructionNode::JumpIfTrue(Default::default())),
+        get_desc(InstructionNode::JumpIfFalse(Default::default())),
+        get_desc(InstructionNode::Jump(Default::default())),
+        get_desc(InstructionNode::SetVar(Default::default())),
+        get_desc(InstructionNode::ReadVar(Default::default())),
+        get_desc(InstructionNode::SubProgram(Default::default())),
+    ]
+}
+
+fn get_desc(node: InstructionNode) -> SubProgram<'static> {
+    match node {
+        InstructionNode::Call(_) | InstructionNode::ScalarLabel(_) | InstructionNode::Exit => {
+            unreachable!()
+        }
+        InstructionNode::Start => subprogram_description!(
             "Start",
             "Start of the program",
             SubProgramType::Instruction,
@@ -48,7 +48,7 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [],
             []
         ),
-        subprogram_description!(
+        InstructionNode::Pass => subprogram_description!(
             "Pass",
             "Do nothing",
             SubProgramType::Instruction,
@@ -56,55 +56,51 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [],
             []
         ),
-        subprogram_description!(
-            "ScalarInt",
-            "Make an integer",
-            SubProgramType::Instruction,
-            [],
-            [Scalar],
-            [i32]
-        ),
-        subprogram_description!(
-            "ScalarFloat",
-            "Make a real number",
-            SubProgramType::Instruction,
-            [],
-            [Scalar],
-            [f32]
-        ),
-        subprogram_description!(
-            "StringLiteral",
-            "Make a text",
-            SubProgramType::Instruction,
-            [],
-            [Scalar],
-            [String]
-        ),
-        subprogram_description!(
-            "JumpIfTrue",
-            "Jump to the input node if the last value is true else do nothing.",
-            SubProgramType::Instruction,
-            [Scalar],
-            [],
-            [NodeId]
-        ),
-        subprogram_description!(
-            "Equals",
-            "Return 1 if the inputs are equal, 0 otherwise",
+        InstructionNode::Add => subprogram_description!(
+            "Add",
+            "Add two scalars",
             SubProgramType::Instruction,
             [Scalar, Scalar],
             [Scalar],
             []
         ),
-        subprogram_description!(
-            "NotEquals",
-            "Return 0 if the inputs are equal, 1 otherwise",
+
+        InstructionNode::Sub => subprogram_description!(
+            "Sub",
+            "Subtract two scalars",
             SubProgramType::Instruction,
             [Scalar, Scalar],
             [Scalar],
             []
         ),
-        subprogram_description!(
+
+        InstructionNode::Mul => subprogram_description!(
+            "Mul",
+            "Multiply two scalars",
+            SubProgramType::Instruction,
+            [Scalar, Scalar],
+            [Scalar],
+            []
+        ),
+
+        InstructionNode::Div => subprogram_description!(
+            "Div",
+            "Divide two scalars",
+            SubProgramType::Instruction,
+            [Scalar, Scalar],
+            [Scalar],
+            []
+        ),
+
+        InstructionNode::CopyLast => subprogram_description!(
+            "CopyLast",
+            "Duplicate the last item on the stack",
+            SubProgramType::Instruction,
+            [Scalar],
+            [Scalar, Scalar],
+            []
+        ),
+        InstructionNode::Less => subprogram_description!(
             "Less",
             "Return 1 if the first input is less than the second, 0 otherwise",
             SubProgramType::Instruction,
@@ -112,7 +108,8 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [Scalar],
             []
         ),
-        subprogram_description!(
+
+        InstructionNode::LessOrEq => subprogram_description!(
             "LessOrEq",
             "Return 1 if the first input is less than or equal to the second, 0 otherwise",
             SubProgramType::Instruction,
@@ -120,7 +117,26 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [Scalar],
             []
         ),
-        subprogram_description!(
+
+        InstructionNode::Equals => subprogram_description!(
+            "Equals",
+            "Return 1 if the inputs are equal, 0 otherwise",
+            SubProgramType::Instruction,
+            [Scalar, Scalar],
+            [Scalar],
+            []
+        ),
+
+        InstructionNode::NotEquals => subprogram_description!(
+            "NotEquals",
+            "Return 0 if the inputs are equal, 1 otherwise",
+            SubProgramType::Instruction,
+            [Scalar, Scalar],
+            [Scalar],
+            []
+        ),
+
+        InstructionNode::Pop => subprogram_description!(
             "Pop",
             "Pops the top elements on the stack and discards it",
             SubProgramType::Instruction,
@@ -128,7 +144,80 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [],
             []
         ),
-        subprogram_description!(
+
+        InstructionNode::ClearStack => subprogram_description!(
+            "ClearStack",
+            "Clears the stack",
+            SubProgramType::Instruction,
+            [],
+            [],
+            []
+        ),
+
+        InstructionNode::ScalarInt(_) => subprogram_description!(
+            "ScalarInt",
+            "Make an integer",
+            SubProgramType::Instruction,
+            [],
+            [Scalar],
+            [i32]
+        ),
+
+        InstructionNode::ScalarFloat(_) => subprogram_description!(
+            "ScalarFloat",
+            "Make a real number",
+            SubProgramType::Instruction,
+            [],
+            [Scalar],
+            [f32]
+        ),
+
+        InstructionNode::ScalarArray(_) => subprogram_description!(
+            "ScalarArray",
+            "Make an array by providing a number and values",
+            SubProgramType::Instruction,
+            [Scalar],
+            [Scalar],
+            []
+        ),
+
+        InstructionNode::StringLiteral(_) => subprogram_description!(
+            "StringLiteral",
+            "Make a text",
+            SubProgramType::Instruction,
+            [],
+            [Scalar],
+            [String]
+        ),
+
+        InstructionNode::JumpIfTrue(_) => subprogram_description!(
+            "JumpIfTrue",
+            "Jump to the input node if the last value is true else do nothing.",
+            SubProgramType::Branch,
+            [Scalar],
+            [],
+            [NodeId]
+        ),
+
+        InstructionNode::JumpIfFalse(_) => subprogram_description!(
+            "JumpIfFalse",
+            "Jump to the input node if the last value is false else do nothing.",
+            SubProgramType::Branch,
+            [Scalar],
+            [],
+            [NodeId]
+        ),
+
+        InstructionNode::Jump(_) => subprogram_description!(
+            "Jump",
+            "Jump to the input node.",
+            SubProgramType::Branch,
+            [],
+            [],
+            [NodeId]
+        ),
+
+        InstructionNode::SetVar(_) => subprogram_description!(
             "SetVar",
             "Sets the value of a variable",
             SubProgramType::Instruction,
@@ -136,7 +225,8 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [],
             [VarName]
         ),
-        subprogram_description!(
+
+        InstructionNode::ReadVar(_) => subprogram_description!(
             "ReadVar",
             "Read the value of a variable",
             SubProgramType::Instruction,
@@ -144,7 +234,7 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [TPointer],
             [VarName]
         ),
-        subprogram_description!(
+        InstructionNode::SubProgram(_) => subprogram_description!(
             "SubProgram",
             "Call a SubProgram by name",
             SubProgramType::Undefined,
@@ -152,5 +242,5 @@ pub fn get_instruction_descriptions() -> Vec<SubProgram<'static>> {
             [],
             [InputString]
         ),
-    ]
+    }
 }
