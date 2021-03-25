37877bb2e597887c03522eaa976fd734bf3c9755
Daniel Kiss <littlesnorrboy@gmail.com>
2021-03-25 17:20:56 +0100

    Remove yucky macro. Thanks const generics <3

--- cao-lang/src/procedures.rs
+++ cao-lang/src/procedures.rs
@@ -168,45 +168,35 @@ fn convert_error<'a, T: 'a>(n: i32) -> impl Fn(T) -> ExecutionError + 'a {
     move |_| ExecutionError::invalid_argument(format!("Failed to convert argument {}", n))
 }
 
-macro_rules! callable_array_arg {
-    ($num: expr) => {
-        impl<Aux, F, T> Callable<Aux> for FunctionWrapper<Aux, F, [T; $num]>
-        where
-            F: Fn(&mut Vm<Aux>, [T; $num]) -> ExecutionResult,
-            T: TryFrom<Scalar>,
-        {
-            fn call(&mut self, vm: &mut Vm<Aux>, constants: &[Scalar]) -> ExecutionResult {
-                use arrayvec::ArrayVec;
+impl<Aux, F, T, const LEN: usize> Callable<Aux> for FunctionWrapper<Aux, F, [T; LEN]>
+where
+    F: Fn(&mut Vm<Aux>, [T; LEN]) -> ExecutionResult,
+    T: TryFrom<Scalar>,
+    [T; LEN]: arrayvec::Array<Item = T>,
+{
+    fn call(&mut self, vm: &mut Vm<Aux>, constants: &[Scalar]) -> ExecutionResult {
+        use arrayvec::ArrayVec;
 
-                let args = constants.iter().enumerate().try_fold(ArrayVec::default(), |mut arr, (i, val)| {
+        let args =
+            constants
+                .iter()
+                .enumerate()
+                .try_fold(ArrayVec::default(), |mut arr, (i, val)| {
                     let i = i as i32;
                     let val = T::try_from(*val).map_err(convert_error(i))?;
                     arr.push(val);
                     Ok(arr)
                 })?;
 
-                let args = args.into_inner().map_err(|_| {
-                    ExecutionError::invalid_argument(None)
-                })?;
+        let args = args
+            .into_inner()
+            .map_err(|_| ExecutionError::invalid_argument(None))?;
 
-                (self.f)(vm, args)
-            }
+        (self.f)(vm, args)
+    }
 
-            fn num_params(&self) -> u8 {
-                $num
-            }
-        }
-    };
-    ($($nums: expr),*) => {
-        $(callable_array_arg!($nums);)*
-    };
+    fn num_params(&self) -> u8 {
+        debug_assert!((LEN & std::u8::MAX as usize) == LEN);
+        LEN as u8
+    }
 }
-
-callable_array_arg!(
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73,
-    74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
-    98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
-    117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128
-);
