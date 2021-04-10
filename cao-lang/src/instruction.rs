fde8a7b788806edca6e3517985a69f450a995f0c
Daniel Kiss <littlesnorrboy@gmail.com>
2021-04-10 19:29:43 +0200

    Use num_enum instead of the hand written hax

--- cao-lang/src/instruction.rs
+++ cao-lang/src/instruction.rs
@@ -1,108 +1,86 @@
-use std::{convert::TryFrom, mem::transmute};
-
 /// Single instruction of the interpreter
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, num_enum::TryFromPrimitive)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[repr(u8)]
 pub enum Instruction {
     /// Add two numbers
-    Add = 0,
+    Add,
     /// Subtract two numbers
-    Sub = 1,
+    Sub,
     /// Multiply two numbers
-    Mul = 2,
+    Mul,
     /// Divide the first number by the second
-    Div = 3,
+    Div,
     /// Call a function provided by the runtime
     /// Requires function name as a string as input
-    Call = 4,
+    Call,
     /// Push an int onto the stack
-    ScalarInt = 5,
+    ScalarInt,
     /// Push a float onto the stack
-    ScalarFloat = 6,
+    ScalarFloat,
     /// Push a label onto the stack
-    ScalarLabel = 7,
+    ScalarLabel,
     /// Pop the next N (positive integer) number of items from the stack and write them to memory
     /// Push the pointer to the beginning of the array onto the stack
-    ScalarArray = 8,
+    ScalarArray,
     /// Writes the strings followed by the instruction to memory and pushes the pointer pointing to
     /// it onto the stack
-    StringLiteral = 9,
+    StringLiteral,
     /// Empty instruction that has no effects
-    Pass = 10,
+    Pass,
     /// Clones the last element on the stack
     /// Does nothing if no elements are on the stack
-    CopyLast = 11,
+    CopyLast,
     /// If the value at the top of the stack is truthy jumps to the input node
     /// Else does nothing
     /// Quit the program returning the last value on the stack
-    Exit = 12,
+    Exit,
     // TODO: replace jump instructions with an instruction to read label as bytecode position and
     // use GOTO instead of jumps...
     /// Jump to the label on top of the stack
-    Jump = 13,
+    Jump,
     /// Compares two scalars
-    Equals = 14,
+    Equals,
     /// Compares two scalars
-    NotEquals = 15,
+    NotEquals,
     /// Is the first param less than the second?
-    Less = 16,
+    Less,
     /// Is the first param less than or equal to the second?
-    LessOrEq = 17,
+    LessOrEq,
     /// Pops the top of the stack and discards it
-    Pop = 18,
+    Pop,
     /// Sets the variable at the top of the stack to the value of the second item on the stack
-    SetGlobalVar = 19,
+    SetGlobalVar,
     /// Reads the variable and pushes its value onto the stack
-    ReadGlobalVar = 20,
+    ReadGlobalVar,
     /// Clears until the last sentinel
-    ClearStack = 21,
+    ClearStack,
     /// If the value at the top of the stack is falsy jumps to the input node
     /// Else does nothing
     /// Insert a history entry
-    Breadcrumb = 22,
+    Breadcrumb,
     /// Push a `null` value onto the stack
-    ScalarNull = 23,
+    ScalarNull,
     /// Returns to right-after-the-last-call-instruction
     /// Also clears the stack until the last sentinel
-    Return = 24,
+    Return,
     /// Pop an offset from the stack and remember the location to that offset from the current
     /// position
-    Remember = 25,
+    Remember,
     /// Starts a new scope
-    ScopeStart = 26,
+    ScopeStart,
     /// Starts a new scope
-    ScopeEnd = 27,
+    ScopeEnd,
     /// Read bytecode position and move there
-    Goto = 28,
+    Goto,
     /// Swaps the last two values on the stack
-    SwapLast = 29,
+    SwapLast,
     /// Pop a scalar from the stack and `goto` there if the value was
     /// truthy
-    GotoIfTrue = 30,
-    And = 31,
-    Or = 32,
-    Xor = 33,
-    Not = 34,
-    GotoIfFalse = 35,
-}
-
-impl Instruction {
-    pub fn is_valid_instr(n: u8) -> bool {
-        n <= 35
-    }
-}
-
-impl TryFrom<u8> for Instruction {
-    type Error = u8;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        if Self::is_valid_instr(value) {
-            // # SAFETY
-            // as long as the values are continous and fit in 8 bits this is fine (tm)
-            unsafe { Ok(transmute(value)) }
-        } else {
-            Err(value)
-        }
-    }
+    GotoIfTrue,
+    And,
+    Or,
+    Xor,
+    Not,
+    GotoIfFalse,
 }
