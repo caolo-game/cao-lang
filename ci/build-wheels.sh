560ab389e8b4e8f22fa9e19104733d6590820b5d
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-05 14:17:03 +0200

    Add build-wheels.sh

---
+++ ci/build-wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+    "${PYBIN}/pip" install -U auditwheel build
+    "${PYBIN}/python" build --wheel
+done
+
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist/
+done
