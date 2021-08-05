bbbb0cafe88cd1b34102667db913e0073d54a495
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-05 14:41:07 +0200

    fix

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -4,8 +4,12 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 source $HOME/.cargo/env
 
-pip install -U auditwheel build
-python -m build --wheel
+cd /cao-lang
+
+for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+    "${PYBIN}/pip" install -U auditwheel build
+    "${PYBIN}/python" -m build --wheel
+done
 
 for whl in dist/*.whl; do
     auditwheel repair "$whl" -w dist/
