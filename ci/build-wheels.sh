f72e10c5261abb9d16c0e7287a23b3b1842f2842
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-05 14:34:02 +0200

    fuck me

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+source $HOME/.cargo/env
+
 pip install -U auditwheel build
 python -m build --wheel
 
