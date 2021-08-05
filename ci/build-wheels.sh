e61e4b98813c9b71f9498f7c63e42c717d9ed48c
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-05 14:43:25 +0200

    fos

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -6,6 +6,8 @@ source $HOME/.cargo/env
 
 cd /cao-lang
 
+ls -al /opt/python
+
 for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
     "${PYBIN}/pip" install -U auditwheel build
     "${PYBIN}/python" -m build --wheel
