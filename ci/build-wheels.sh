9c1ff74a3e4d19c3c9660c07ea98404280225e24
Daniel Kiss <littlesnorrboy@gmail.com>
2022-01-30 23:22:26 +0100

    Add PyPy to the publish pipeline

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -6,16 +6,8 @@ source $HOME/.cargo/env
 
 cd /cao-lang
 
-ls -al /opt/python
-
 # build cpython wheels
 for PYBIN in /opt/python/cp*/bin; do
-    "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
-    "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
-done
-
-# build pypy wheels
-for PYBIN in /opt/python/pypy*/bin; do
     "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
     "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
 done
@@ -24,3 +16,10 @@ for whl in dist/*.whl; do
     auditwheel repair "$whl" -w dist/
     rm "$whl"
 done
+
+# build pypy wheels
+for PYBIN in /opt/python/pypy*/bin; do
+    "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
+    "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
+done
+
