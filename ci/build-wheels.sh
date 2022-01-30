6a6a9ec6d1ac5b1d900cfa6bcf18a162f75a8138
Daniel Kiss <littlesnorrboy@gmail.com>
2022-01-30 23:07:44 +0100

    Initialize pypy support

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -8,7 +8,14 @@ cd /cao-lang
 
 ls -al /opt/python
 
+# build cpython wheels
 for PYBIN in /opt/python/cp*/bin; do
+    "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
+    "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
+done
+
+# build pypy wheels
+for PYBIN in /opt/python/pypy*/bin; do
     "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
     "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
 done
