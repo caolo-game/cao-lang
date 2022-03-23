76f225b6c2e21abf9d3418bb10f336d22adc7833
Daniel Kiss <littlesnorrboy@gmail.com>
2022-03-23 23:39:56 +0100

    Attempt to fix manylinux CI builds

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -6,10 +6,21 @@ source $HOME/.cargo/env
 
 cd /cao-lang
 
+function build_for_py {
+    local pybin=$1
+    # minimum 3.7 supported
+    local version_test
+    version_test=$(${pybin}/python -c 'import sys; print(sys.version_info.minor)' 2>/dev/null)
+
+    if [[ $version_test -ge "7" ]]; then 
+        "${pybin}/pip" install -U auditwheel setuptools_rust wheel toml
+        "${pybin}/python" setup.py build bdist_wheel --py-limited-api=cp35
+    fi
+}
+
 # build cpython wheels
-for PYBIN in /opt/python/cp*/bin; do
-    "${PYBIN}/pip" install -U auditwheel setuptools_rust wheel toml
-    "${PYBIN}/python" setup.py build bdist_wheel --py-limited-api=cp35
+for pybin in /opt/python/cp*/bin; do
+    build_for_py $pybin
 done
 
 for whl in dist/*.whl; do
