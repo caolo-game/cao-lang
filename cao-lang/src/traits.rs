0511d1bb0fdb5212711fd7621682fb01138ded09
Daniel Kiss <littlesnorrboy@gmail.com>
2022-10-31 14:17:36 +0100

    Remove function name from the output

--- cao-lang/src/traits.rs
+++ cao-lang/src/traits.rs
@@ -78,15 +78,10 @@ where
     }
 }
 
-fn conversion_error(
-    function: &str,
-    input: usize,
-    expected: &str,
-    actual: &str,
-) -> ExecutionErrorPayload {
+fn conversion_error(input: usize, expected: &str, actual: &str) -> ExecutionErrorPayload {
     ExecutionErrorPayload::invalid_argument(format!(
-        "Failed to convert function ({}) input #{}: Expected: {}, Found: {}",
-        function, input, expected, actual
+        "Failed to convert function input #{}: Expected: {}, Found: {}",
+        input, expected, actual
     ))
 }
 
@@ -96,9 +91,8 @@ where
 {
     fn call(&self, vm: &mut Vm<Aux>) -> ShallowExecutionResult {
         let v1 = vm.stack_pop();
-        let v1 = T1::try_from(v1).map_err(|_| {
-            conversion_error(type_name::<Self>(), 1, type_name::<T1>(), v1.type_name())
-        })?;
+        let v1 =
+            T1::try_from(v1).map_err(|_| conversion_error(1, type_name::<T1>(), v1.type_name()))?;
         self(vm, v1)
     }
 }
@@ -110,13 +104,11 @@ where
 {
     fn call(&self, vm: &mut Vm<Aux>) -> ShallowExecutionResult {
         let v2 = vm.stack_pop();
-        let v2 = T2::try_from(v2).map_err(|_| {
-            conversion_error(type_name::<Self>(), 2, type_name::<T2>(), v2.type_name())
-        })?;
+        let v2 =
+            T2::try_from(v2).map_err(|_| conversion_error(2, type_name::<T2>(), v2.type_name()))?;
         let v1 = vm.stack_pop();
-        let v1 = T1::try_from(v1).map_err(|_| {
-            conversion_error(type_name::<Self>(), 1, type_name::<T1>(), v1.type_name())
-        })?;
+        let v1 =
+            T1::try_from(v1).map_err(|_| conversion_error(1, type_name::<T1>(), v1.type_name()))?;
         self(vm, v1, v2)
     }
 }
@@ -129,17 +121,14 @@ where
 {
     fn call(&self, vm: &mut Vm<Aux>) -> ShallowExecutionResult {
         let v3 = vm.stack_pop();
-        let v3 = T3::try_from(v3).map_err(|_| {
-            conversion_error(type_name::<Self>(), 3, type_name::<T3>(), v3.type_name())
-        })?;
+        let v3 =
+            T3::try_from(v3).map_err(|_| conversion_error(3, type_name::<T3>(), v3.type_name()))?;
         let v2 = vm.stack_pop();
-        let v2 = T2::try_from(v2).map_err(|_| {
-            conversion_error(type_name::<Self>(), 2, type_name::<T2>(), v2.type_name())
-        })?;
+        let v2 =
+            T2::try_from(v2).map_err(|_| conversion_error(2, type_name::<T2>(), v2.type_name()))?;
         let v1 = vm.stack_pop();
-        let v1 = T1::try_from(v1).map_err(|_| {
-            conversion_error(type_name::<Self>(), 1, type_name::<T1>(), v1.type_name())
-        })?;
+        let v1 =
+            T1::try_from(v1).map_err(|_| conversion_error(1, type_name::<T1>(), v1.type_name()))?;
         self(vm, v1, v2, v3)
     }
 }
@@ -154,21 +143,17 @@ where
 {
     fn call(&self, vm: &mut Vm<Aux>) -> ShallowExecutionResult {
         let v4 = vm.stack_pop();
-        let v4 = T4::try_from(v4).map_err(|_| {
-            conversion_error(type_name::<Self>(), 4, type_name::<T4>(), v4.type_name())
-        })?;
+        let v4 =
+            T4::try_from(v4).map_err(|_| conversion_error(4, type_name::<T4>(), v4.type_name()))?;
         let v3 = vm.stack_pop();
-        let v3 = T3::try_from(v3).map_err(|_| {
-            conversion_error(type_name::<Self>(), 3, type_name::<T3>(), v3.type_name())
-        })?;
+        let v3 =
+            T3::try_from(v3).map_err(|_| conversion_error(3, type_name::<T3>(), v3.type_name()))?;
         let v2 = vm.stack_pop();
-        let v2 = T2::try_from(v2).map_err(|_| {
-            conversion_error(type_name::<Self>(), 2, type_name::<T2>(), v2.type_name())
-        })?;
+        let v2 =
+            T2::try_from(v2).map_err(|_| conversion_error(2, type_name::<T2>(), v2.type_name()))?;
         let v1 = vm.stack_pop();
-        let v1 = T1::try_from(v1).map_err(|_| {
-            conversion_error(type_name::<Self>(), 1, type_name::<T1>(), v1.type_name())
-        })?;
+        let v1 =
+            T1::try_from(v1).map_err(|_| conversion_error(1, type_name::<T1>(), v1.type_name()))?;
         self(vm, v1, v2, v3, v4)
     }
 }
