50e93dc211179b009b40d3dd1425673a874e776e
Daniel Kiss <littlesnorrboy@gmail.com>
2023-07-14 23:09:36 +0200

    Fix warning

--- cao-lang/src/collections/hash_map.rs
+++ cao-lang/src/collections/hash_map.rs
@@ -424,7 +424,7 @@ impl<K, V, A: Allocator> CaoHashMap<K, V, A> {
             unsafe {
                 debug_assert!(ind < len);
                 let h = hashes[ind];
-                if h == 0 || (h == needle && (*keys.add(ind)).borrow() == k.borrow()) {
+                if h == 0 || (h == needle && (*keys.add(ind)).borrow() == k) {
                     return ind;
                 }
             }
