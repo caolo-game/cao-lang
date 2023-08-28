f011d654996c57a97061c1c47bfc2761f5a40c10
Daniel Kiss <littlesnorrboy@gmail.com>
2023-08-29 01:16:32 +0200

    Use git for fetching gtest

--- c/cmake/FindGoogleTest.cmake
+++ c/cmake/FindGoogleTest.cmake
@@ -2,7 +2,9 @@ include(FetchContent)
 
 FetchContent_Declare(
   googletest
-  URL "https://github.com/google/googletest/archive/release-1.11.0.tar.gz")
+  GIT_REPOSITORY "https://github.com/google/googletest"
+  GIT_TAG "f8d7d77c06936315286eb55f8de22cd23c188571"
+)
 
 # Prevent overriding the parent project's compiler/linker settings on Windows
 set(gtest_force_shared_crt
@@ -15,6 +17,6 @@ FetchContent_MakeAvailable(googletest)
 
 FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
-  FetchContent_Populate(googletest)
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+    FetchContent_Populate(googletest)
+    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
 endif()
