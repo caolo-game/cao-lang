99a56aa58a2565796a32d2ece94f837f9f4e936b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-06-09 17:18:34 +0200

    feat: Add console log native function to wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -73,14 +73,44 @@ pub struct RunResult {}
 #[wasm_bindgen]
 impl RunResult {}
 
+fn cao_lang_log(_vm: &mut Vm<()>, val: Value) -> Result<Value, ExecutionErrorPayload> {
+    match val {
+        Value::Nil => log::info!("Console log: nil"),
+        Value::Object(o) => unsafe {
+            let o = o.as_ref();
+            match &o.body {
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::String(s) => {
+                    let pl = s.as_str();
+                    log::info!("Console log: {pl}");
+                }
+                // TODO: log recursively
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::Table(_) => todo!(),
+                // TODO: more information
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::Function(_) => todo!(),
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::NativeFunction(_) => {
+                    todo!()
+                }
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::Closure(_) => todo!(),
+                cao_lang::vm::runtime::cao_lang_object::CaoLangObjectBody::Upvalue(_) => todo!(),
+            }
+        },
+        Value::Integer(pl) => log::info!("Console log: {pl}"),
+        Value::Real(pl) => log::info!("Console log: {pl}"),
+    }
+    Ok(Value::Nil)
+}
+
 /// Runs the given compiled Cao-Lang program (output of `compile`).
 ///
 /// Will run in a 'plain' Vm, no custom methods will be available!
 #[wasm_bindgen(js_name = "runProgram")]
 pub fn run_program(program: JsValue) -> Result<RunResult, JsValue> {
     let mut vm = Vm::new(()).expect("Failed to initialize VM");
+    vm.register_native_stdlib().expect("Failed to init stdlib");
+    vm.register_native_function("log", into_f1(cao_lang_log))
+        .expect("Failed to register log function");
     let program: CaoCompiledProgram = serde_wasm_bindgen::from_value(program).map_err(err_to_js)?;
-    vm.run(&program).map_err(err_to_js).map(|()| RunResult {})
+    vm.run(&program).map_err(err_to_js).map(|_| RunResult {})
 }
 
 fn err_to_js(e: impl std::error::Error) -> JsValue {
